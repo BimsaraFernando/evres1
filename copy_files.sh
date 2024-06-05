f2b6a3e592de88d49d0575919bdab5619ce2c291
BimsaraFernando <kavindu@geveo.com>
2024-06-05 14:09:39 +0530

    sashi_v3.5.20 kavindu80

--- copy_files.sh
+++ copy_files.sh
@@ -1,16 +1,16 @@
-# ./sashi_release.sh v3.5.19 main kavindu79
-#curl -fsSL https://github.com/BimsaraFernando/evres1/releases/download/sashi_v3.5.19/setup.sh | cat | sudo SKIP_SYSREQ=1 NO_DOMAIN=1 NETWORK=devnet bash -s install
+# ./sashi_release.sh v3.5.20 main kavindu80
+#curl -fsSL https://github.com/BimsaraFernando/evres1/releases/download/sashi_v3.5.20/setup.sh | cat | sudo SKIP_SYSREQ=1 NO_DOMAIN=1 NETWORK=devnet bash -s install
 #!/bin/bash
 
 # Define an array of source and destination file paths
 file_paths=(
-    "/home/kavindu/Documents/Evernode/sashimono/sashimono/build/installer.tar.gz /home/kavindu/Documents/Evernode/release-Kavindu/evres1/installer/installer.tar.gz"
-    "/home/kavindu/Documents/Evernode/sashimono/sashimono/build/setup-jshelper.tar.gz /home/kavindu/Documents/Evernode/release-Kavindu/evres1/installer/setup-jshelper.tar.gz"
-    "/home/kavindu/Documents/Evernode/sashimono/sashimono/installer/setup.sh /home/kavindu/Documents/Evernode/release-Kavindu/evres1/installer/setup.sh"
-    "/home/kavindu/Documents/Evernode/sashimono/sashimono/build/reputation-contract.tar.gz /home/kavindu/Documents/Evernode/release-Kavindu/evres1/installer/reputation-contract.tar.gz"
+    "/home/kavindu/Documents/Evernode/sashimono2/sashimono/build/installer.tar.gz /home/kavindu/Documents/Evernode/release-Kavindu/evres1/installer/installer.tar.gz"
+    "/home/kavindu/Documents/Evernode/sashimono2/sashimono/build/setup-jshelper.tar.gz /home/kavindu/Documents/Evernode/release-Kavindu/evres1/installer/setup-jshelper.tar.gz"
+    "/home/kavindu/Documents/Evernode/sashimono2/sashimono/installer/setup.sh /home/kavindu/Documents/Evernode/release-Kavindu/evres1/installer/setup.sh"
+    "/home/kavindu/Documents/Evernode/sashimono2/sashimono/build/reputation-contract.tar.gz /home/kavindu/Documents/Evernode/release-Kavindu/evres1/installer/reputation-contract.tar.gz"
 )
 
-cd "/home/kavindu/Documents/Evernode/sashimono/sashimono/" &&  make -j8 && make installer -j8
+cd "/home/kavindu/Documents/Evernode/sashimono2/sashimono/" &&  make -j8 && make installer -j8
 
 # Loop through each pair and perform the copy and replace operation
 for path_pair in "${file_paths[@]}"; do
