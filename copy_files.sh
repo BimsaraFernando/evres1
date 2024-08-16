6f45b2950be3bef219746aa9af0db1bd8c8ff629
BimsaraFernando <kavindu@geveo.com>
2024-08-16 12:27:09 +0530

    sashi_v3.7.0 kavindu129

--- copy_files.sh
+++ copy_files.sh
@@ -1,5 +1,5 @@
-# ./sashi_release.sh v3.6.28 main kavindu128
-#curl -fsSL https://github.com/BimsaraFernando/evres1/releases/download/sashi_v3.6.28/setup.sh | cat | sudo SKIP_SYSREQ=1 NO_DOMAIN=1 NETWORK=devnet bash -s install
+# ./sashi_release.sh v3.7.0 main kavindu129
+#curl -fsSL https://github.com/BimsaraFernando/evres1/releases/download/sashi_v3.7.0/setup.sh | cat | sudo SKIP_SYSREQ=1 NO_DOMAIN=1 NETWORK=devnet bash -s install
 #!/bin/bash
 
 # Define an array of source and destination file paths
